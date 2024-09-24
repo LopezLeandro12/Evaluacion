8f068aa32f728119f463dbba0631f306023fe43f
lopez <lopez@DESKTOP-T3SVIFR>
2024-09-24 14:07:14 -0300

    Agregar archivos de proyecto.

---
+++ Evaluacion/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Evaluacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_Titulo = new System.Windows.Forms.Label();
+            this.lbl_nombre_evo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(802, 23);
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(439, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Evaluacion.Properties.Resources.WhatsApp_Image_2024_09_23_at_10_29_32_PM;
+            this.pictureBox1.Location = new System.Drawing.Point(256, 101);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(330, 228);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // lbl_Titulo
+            // 
+            this.lbl_Titulo.AutoSize = true;
+            this.lbl_Titulo.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_Titulo.Font = new System.Drawing.Font("Impact", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Titulo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_Titulo.Location = new System.Drawing.Point(253, 66);
+            this.lbl_Titulo.Name = "lbl_Titulo";
+            this.lbl_Titulo.Size = new System.Drawing.Size(143, 28);
+            this.lbl_Titulo.TabIndex = 3;
+            this.lbl_Titulo.Text = "Digivolution!!!";
+            // 
+            // lbl_nombre_evo
+            // 
+            this.lbl_nombre_evo.AutoSize = true;
+            this.lbl_nombre_evo.Font = new System.Drawing.Font("Impact", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nombre_evo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_nombre_evo.Location = new System.Drawing.Point(387, 343);
+            this.lbl_nombre_evo.Name = "lbl_nombre_evo";
+            this.lbl_nombre_evo.Size = new System.Drawing.Size(0, 28);
+            this.lbl_nombre_evo.TabIndex = 4;
+            this.lbl_nombre_evo.Click += new System.EventHandler(this.lbl_nombre_evo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_nombre_evo);
+            this.Controls.Add(this.lbl_Titulo);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_Titulo;
+        private System.Windows.Forms.Label lbl_nombre_evo;
+    }
+}
+
---
+++ Evaluacion/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluacion
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartProgress(); 
+               
+ 
+          
+                
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private async void StartProgress()
+        {
+            progressBar1.Value = 0;
+            lbl_nombre_evo.Text = "V-Mon!";
+
+            while (progressBar1.Value < 100)
+            {
+                progressBar1.Value++;
+
+                if (progressBar1.Value < 45)
+                {
+                    pictureBox1.Image = Properties.Resources.WhatsApp_Image_2024_09_23_at_10_29_32_PM;
+                }
+                else if (progressBar1.Value < 75)
+                {
+                    pictureBox1.Image = Properties.Resources.WhatsApp_Image_2024_09_23_at_10_31_02_PM;
+                }
+                else
+                {
+                    pictureBox1.Image = Properties.Resources.WhatsApp_Image_2024_09_23_at_10_35_21_PM;
+                }
+
+                if (progressBar1.Value == 45)
+                {
+                    lbl_nombre_evo.Text = "XV-Mon!!!";
+                }
+                else if (progressBar1.Value == 75)
+                {
+                    lbl_nombre_evo.Text = "Paildramon!!!";
+
+                }
+                await Task.Delay(500); 
+            }
+        }
+
+       
+
+        private void lbl_nombre_evo_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
